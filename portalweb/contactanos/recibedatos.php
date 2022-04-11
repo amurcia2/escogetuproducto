16647ecc1d615b423f9f2371a8d94301dd5df0ee
amurcia2 <96141437+amurcia2@users.noreply.github.com>
2022-04-11 19:43:44 +0200

    Add files via upload

---
+++ portalweb/contactanos/recibedatos.php
@@ -0,0 +1,17 @@
+<?php
+  $nombre =$_POST['nombre'];
+  $apellidos=$_POST['apellidos'];
+  $edad=$_POST['edad'];
+  
+  $sexo=$_POST['sexo'];
+  $usuario=$_POST['usuario'];
+  $contrseña=$_POST['contrasena'];
+  $confirmar=$_POST['confirmar'];
+  echo "el nombre es: $nombre </br>";
+  echo "el apellido es: $apellido </br>";
+  echo "el edad es: $edad </br>";
+  echo "el sexo es: $sexo </br>";
+  echo "el usuario es: $usuario </br>";
+  echo "el sexo contrasena: $contrasena </br>";
+  echo "el confirmar es: $confirmar </br>";
+?>
